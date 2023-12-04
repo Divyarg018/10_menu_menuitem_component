ed690cedcf4adc0b1b1b25efb2cce458c61ddeaf
Divya R <kwrdivyaraj100@gmail.com>
2023-12-04 16:44:23 +0530

    Material UI Menu & MenuItem Component

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import { Button, Menu, MenuItem } from '@mui/material';
 
 function Test() {
-    const [anchorE1m, setAnchorE1m] = useState(null);
+    const [anchorElm, setAnchorElm] = useState(null);
     const [open, setOpen] = useState(false);
+
     const handleClose = () => {
-        setAnchorE1m(null);
+        setAnchorElm(null);
         setOpen(false);
     }
 
     const handleClick = (e) => {
-        setAnchorE1m(e.currentTarget);
+        setAnchorElm(e.currentTarget);
         setOpen(true);
-
     };
 
     return (
         <div>
             <Button variant='contained' onClick={handleClick}>Open Menu</Button>
             <Button onClick={handleClick}>Open Menu Again</Button>
-            <Menu anchorE1={anchorE1m} open={open} onClose={handleClose}>
+            <Menu anchorEl={anchorElm} open={open} onClose={handleClose}>
                 <MenuItem onClick={handleClose}>Profile</MenuItem>
                 <MenuItem onClick={handleClose}>Balance</MenuItem>
                 <MenuItem onClick={handleClose}>Logout</MenuItem>
